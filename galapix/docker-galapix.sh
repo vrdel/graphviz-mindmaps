ff57b6a26798b26819af1b06e637306607664ef4
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-11-18 18:07:40 +0100

    added galapix rpm and container build instructions

---
+++ galapix/docker-galapix.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+IFS=""
+CONTNAME="galapix"
+
+function run_existing_container()
+{
+	docker exec ${CONTNAME} $*
+}
+
+function run_new_container()
+{
+	docker run \
+	--cap-add=SYS_ADMIN \
+	--device /dev/snd \
+	--device /dev/dri \
+	-e "DISPLAY=unix$DISPLAY" \
+	--log-driver json-file \
+	--log-opt max-size=10m \
+	-v $HOME/:/home/user/ \
+	-v /dev/log:/dev/log \
+	-v /dev/dri:/dev/dri \
+	-v /dev/snd:/dev/snd \
+	-v /etc/localtime:/etc/localtime \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $HOME/.zsh_history:/home/user/.zsh_history \
+	-h docker-centos7 \
+	--net host \
+	--name ${CONTNAME} \
+	--rm \
+	ipanema:5000/galapix:latest $*
+}
+
+if docker ps -f name=${CONTNAME} -q | grep -q '[0-9a-z]*'
+then
+	run_existing_container $*
+else
+	run_new_container $*
+fi
