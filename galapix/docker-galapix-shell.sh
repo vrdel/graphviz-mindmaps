c2a44732b5529a6ed1333fbccbd42f0a0eef1ece
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-11-19 20:28:46 +0100

    no need for inspect of container already running for only shell thingie

--- galapix/docker-galapix-shell.sh
+++ galapix/docker-galapix-shell.sh
@@ -3,37 +3,23 @@
 IFS=""
 CONTNAME="galapix"
 
-function run_existing_container()
-{
-	docker exec ${CONTNAME} $*
-}
-
-function run_new_container()
-{
-	docker rm ${CONTNAME} &>/dev/null; \
-	docker run \
-	--cap-add=SYS_ADMIN \
-	--device /dev/snd \
-	--device /dev/dri \
-	-e "DISPLAY=unix$DISPLAY" \
-	--log-driver json-file \
-	--log-opt max-size=10m \
-	-v /dev/log:/dev/log \
-	-v /dev/dri:/dev/dri \
-	-v /dev/snd:/dev/snd \
-	-v /etc/localtime:/etc/localtime \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v $HOME/.zsh_history:/home/user/.zsh_history \
-	-h docker-galapix \
-	--net host \
-	--name ${CONTNAME} \
-	--rm -ti \
-	ipanema:5000/galapix:latest /bin/zsh
-}
-
-if docker ps -f name=${CONTNAME} -f status=running -q | grep -q '[0-9a-z]*'
-then
-	run_existing_container $*
-else
-	run_new_container $*
-fi
+docker rm ${CONTNAME} &>/dev/null; \
+docker run \
+--cap-add=SYS_ADMIN \
+--device /dev/snd \
+--device /dev/dri \
+-e "DISPLAY=unix$DISPLAY" \
+--log-driver json-file \
+--log-opt max-size=10m \
+-v /dev/log:/dev/log \
+-v /dev/dri:/dev/dri \
+-v /dev/snd:/dev/snd \
+-v /etc/localtime:/etc/localtime \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $HOME/.zsh_history:/home/user/.zsh_history \
+-h docker-galapix \
+--net host \
+-u user \
+--name ${CONTNAME} \
+--rm -ti \
+ipanema:5000/galapix:latest /bin/zsh
