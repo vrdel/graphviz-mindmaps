006809efff2977fb29261ae706b923d40e4673a9
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-04-20 13:04:52 +0200

    remove giga pixel viewer and helpers

--- rofi-galapix/rofi_galapixes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/my_notes/galapix/
-
-find -L . -type f -printf '%T@ %p\n' | sort -nr | cut -d' ' -f2- | \
-~/.bin/rofi/bin/rofi -i -p "Old Galapixes" -theme gruvbox-dark-soft -dmenu -multi-select \
-| xargs -d '\n' -I {} "echo" {} | xargs -d '\n' galaview.sh
--- rofi-galapix/rofi_mywhiteboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/my_notes/my-whiteboard/
-
-find -L . -type f -printf '%T@ %p\n' | sort -nr | cut -d' ' -f2- | \
-~/.bin/rofi/bin/rofi -i -p "My Whiteboards" -theme gruvbox-dark-soft -dmenu -multi-select \
-| xargs -d '\n' -I {} "echo" {} | xargs -d '\n' galaview.sh
