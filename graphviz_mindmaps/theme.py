0cbcf99073101371818f9debee3826ad1e6861ab
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-05-06 09:22:45 +0200

    nord theme in palettes as well

--- graphviz_mindmaps/theme.py
+++ graphviz_mindmaps/theme.py
@@ -175,116 +175,31 @@ def _apply_palette(palette):
     return palette["bg"]
 
 
-def _apply_nord():
-    default_bgcolor = "#2e3440"
-
-    constants.fontcolor.update({
-        "def": "#eceff4",
-        "r": "#bf616a",
-        "g": "#a3be8c",
-        "b": "#81a1c1",
-        "y": "#ebcb8b",
-        "c": "#88c0d0",
-        "p": "#b48ead",
-        "k": "#2e3440",
-        "t": "#eceff4",
-    })
-
-    constants.linecolors.update({
-        "r": "#bf616a",
-        "g": "#a3be8c",
-        "b": "#81a1c1",
-        "y": "#ebcb8b",
-        "c": "#8fbcbb",
-        "p": "#b48ead",
-        "k": "#2e3440",
-    })
-
-    constants.vrbtcolors.update({
-        "cgreen": "#35483b",
-        "cred": "#4c3438",
-        "cblue": "#344154",
-        "ccyan": "#34494d",
-        "cyello": "#4d4734",
-        "corang": "#4d3d34",
-        "cpink": "#47394d",
-        "cwhite": "#eceff4",
-        "def": "#3b4252",
-    })
-
-    constants.nodetype.update({
-        "root": "fontsize=\"%s\" margin=\"0.5\" shape=cds style=radial color=\"#81a1c1\" fillcolor=\"#3b4252\" gradientangle=\"90\" fontcolor=\"#eceff4\"" % constants.fontsize["xxl"],
-        "def": "shape=underline fontcolor=\"#eceff4\" color=\"#4c566a\"",
-        "underl": "shape=underline fontcolor=\"#eceff4\" color=\"#4c566a\"",
-        "node": _node("#3b4252", fontcolor="#eceff4"),
-        "list": _node("#334450", shape="rect", style="radial", fontcolor="#eceff4"),
-        "data": _node("#3b4252", shape="cylinder", style="radial", fontcolor="#eceff4", margin="0.1,0.3"),
-        "answer": _node("#3f5142", shape="cds", style="radial", fontcolor="#eceff4", margin="0.2"),
-        "quest": _node("#4a4634", shape="oval", style="radial", fontcolor="#eceff4", margin="0,0"),
-        "title": "shape=doubleoctagon fontname=\"%s\" margin=\"0,0\" fontsize=\"%s\" style=\"radial\" fillcolor=\"#5e81ac\" color=\"#81a1c1\" fontcolor=\"#eceff4\"" % (constants.font["comicb"], constants.fontsize["l"]),
-        "impor": "shape=signature fontsize=\"%s\" margin=\"0.25\" style=\"radial\" fillcolor=\"#5b3d43\" color=\"#bf616a\" fontcolor=\"#eceff4\"" % constants.fontsize["l"],
-        "impog": "shape=signature fontsize=\"%s\" margin=\"0.25\" style=\"radial\" fillcolor=\"#3f5142\" color=\"#a3be8c\" fontcolor=\"#eceff4\"" % constants.fontsize["l"],
-        "impob": "shape=signature fontsize=\"%s\" margin=\"0.25\" style=\"radial\" fillcolor=\"#344154\" color=\"#81a1c1\" fontcolor=\"#eceff4\"" % constants.fontsize["l"],
-        "img": _node("#3b4252", style="radial", fontcolor="#eceff4"),
-        "imgil": _node("#3b4252", style="radial", fontcolor="#eceff4"),
-        "dood": "shape=underline fontcolor=\"#eceff4\" color=\"#4c566a\"",
-        "date": "shape=component gradientangle=\"270\" style=\"filled\" margin=\"0.15,0.15,0.15\" fillcolor=\"#3b4252;0.93:#bf616a\" color=\"#4c566a\" fontcolor=\"#eceff4\"",
-        "link": "shape=component gradientangle=\"270\" style=\"filled\" margin=\"0.15,0.15,0.15\" fillcolor=\"#3b4252;0.93:#5e81ac\" color=\"#4c566a\" fontcolor=\"#eceff4\"",
-        "example": "shape=note fontname=\"%s\" gradientangle=\"270\" style=\"filled\" margin=\"0.15,0.15\" fillcolor=\"#3f5142;0.15:#3b4252\" color=\"#4c566a\" fontcolor=\"#eceff4\"" % constants.font["mono"],
-        "draw": _component(constants.vrbtcolors["cwhite"], fontname=constants.font["mono"]),
-        "verbatim": _component(constants.vrbtcolors["def"], fontname=constants.font["mono"]),
-        "commen": "shape=note fontname=\"%s\" fontsize=\"%s\" margin=\"0.2\" style=\"radial\" fillcolor=\"#4a4634\" color=\"#4c566a\" fontcolor=\"#eceff4\"" % (constants.font["comic"], constants.fontsize["l"]),
-        "term": "shape=note fontname=\"%s\" gradientangle=\"270\" style=\"filled\" margin=\"0.15,0.15\" fillcolor=\"#3b4252\" color=\"#4c566a\" fontcolor=\"#eceff4\"" % constants.font["mono"],
-        "check": "shape=rarrow margin=\"0.20\" style=\"filled\" fillcolor=\"#5e81ac\" fontcolor=\"#eceff4\" color=\"#5e81ac\"",
-        "todo": "shape=box margin=\"0.20\" style=\"filled, diagonals\" fillcolor=\"#4a4634\" fontcolor=\"#eceff4\" color=\"#ebcb8b\"",
-        "decisi": _node("#4d3d34", shape="diamond", fontcolor="#eceff4"),
-        "saying": _node("#34494d", shape="egg", style="radial", fontcolor="#eceff4", margin="0.0,0.15"),
-        "cgreen": _node("#3f5142", fontcolor="#eceff4"),
-        "ccyan": _node("#34494d", fontcolor="#eceff4"),
-        "cblue": _node("#344154", fontcolor="#eceff4"),
-        "cpink": _node("#47394d", fontcolor="#eceff4"),
-        "cred": _node("#5b3d43", fontcolor="#eceff4"),
-        "cyello": _node("#4a4634", fontcolor="#eceff4"),
-        "corang": _node("#4d3d34", fontcolor="#eceff4"),
-        "cgrey": _node("#4c566a", fontcolor="#eceff4"),
-        "cblack": _node("#2e3440", fontcolor="#eceff4"),
-    })
-
-    constants.edgetype.update({
-        "impor": "style=\"bold\" color=\"#bf616a\"",
-        "impog": "style=\"bold\" color=\"#a3be8c\"",
-        "impob": "style=\"bold\" color=\"#81a1c1\"",
-        "cred": "color=\"#bf616a\"",
-        "cgreen": "color=\"#a3be8c\"",
-        "cblue": "color=\"#81a1c1\"",
-        "ccyan": "color=\"#88c0d0\"",
-        "cyello": "color=\"#ebcb8b\"",
-        "cpink": "color=\"#b48ead\"",
-        "corang": "color=\"#d08770\"",
-    })
-
-    constants.edgecolors.update({
-        "r": "color=\"#bf616a\"",
-        "g": "color=\"#a3be8c\"",
-        "b": "color=\"#81a1c1\"",
-        "c": "color=\"#88c0d0\"",
-        "y": "color=\"#ebcb8b\"",
-        "p": "color=\"#b48ead\"",
-    })
-    constants.arrcolors.update({
-        "d": "#d8dee9",
-        "r": "#bf616a",
-        "g": "#a3be8c",
-        "b": "#81a1c1",
-        "y": "#ebcb8b",
-        "c": "#88c0d0",
-        "p": "#b48ead",
-    })
-
-    return default_bgcolor
-
-
 THEME_PALETTES = {
+    "nord": {
+        "bg": "#2e3440",
+        "fg": "#eceff4",
+        "panel": "#3b4252",
+        "border": "#4c566a",
+        "muted": "#4c566a",
+        "accent": "#5e81ac",
+        "red": "#bf616a",
+        "green": "#a3be8c",
+        "blue": "#81a1c1",
+        "yellow": "#ebcb8b",
+        "cyan": "#88c0d0",
+        "purple": "#b48ead",
+        "orange": "#d08770",
+        "red_bg": "#5b3d43",
+        "green_bg": "#3f5142",
+        "blue_bg": "#344154",
+        "yellow_bg": "#4a4634",
+        "cyan_bg": "#34494d",
+        "purple_bg": "#47394d",
+        "orange_bg": "#4d3d34",
+        "white_bg": "#eceff4",
+        "verbatim_bg": "#3b4252",
+    },
     "papercolor": {
         "bg": "#eeeeee",
         "fg": "#444444",
@@ -411,9 +326,6 @@ THEME_PALETTES = {
 def ApplyTheme(name):
     if name == "default":
         return _restore_defaults()
-    if name == "nord":
-        _restore_defaults()
-        return _apply_nord()
     if name in THEME_PALETTES:
         _restore_defaults()
         return _apply_palette(THEME_PALETTES[name])
